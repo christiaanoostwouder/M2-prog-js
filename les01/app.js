2745cea3100930ac86c44b15c62f004e5324357f
christiaanoostwouder <36211@ma-web.nl>
2022-11-24 10:22:14 +0100

    'les01'

---
+++ les01/app.js
@@ -0,0 +1,18 @@
+class App
+{
+    runApplication()
+    {
+        console.log("hello world!");
+        let appNaam= "mijnApp"
+        let versienummer = 0.9
+        let versiedatum = "24/11/2022"
+        let autheur = "C.oostwouder"
+        let copyright = "stemple"
+        let distributeur = "me"
+        let darkmode = false
+
+    }
+}
+
+let app = new App();
+app.runApplication();
