d0610528cd9f41392e74c4c45664782caf374663
christiaan Oostwouder <36211@ma-web.nl>
2022-12-15 10:09:58 +0100

    second commit

---
+++ classes/classstappen.js
@@ -0,0 +1,18 @@
+class Auto
+{
+    constructor(kleur, snelheid, tank, schade)
+    {
+        this.kleur = kleur;
+        this.snelheid = snelheid;
+        this.tank = tank;
+        this.schade = schade;
+    }
+
+    starten()
+    {
+       
+    }
+}
+
+let auto = new Auto()
+auto.starten()
